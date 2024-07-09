047d16dae13bacc97ff11dd7189b0816d7c5fb12
Narasimha1506 <anilkuamr.boddeti@gmail.com>
2024-07-09 12:52:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@
 // import { ImageComponent } from "./components/images/images.js";
 // import {Heading1} from "./components/heading/heading.js";
 // import{ListParentOrdered} from "./components/list/list.js";
-import { Card } from "./components/cards/card.js";
+// import { Card } from "./components/cards/card.js";
+// const App =() => {
+//     // const wishes ="hii anil"
+//     return (
+//         <div>
+//             {/* <h1>Hello World</h1>
+//         <ButtonComponent></ButtonComponent>
+//         <ImageComponent></ImageComponent>
+//         <Heading1></Heading1>
+//          <ListParentOrdered></ListParentOrdered> */}
+//          {/* <h3>{wishes}</h3> */}
+//          {[1].map((num,index)=>(<Card key={index}></Card>))}
+//          {/* {false ? <Card></Card> : null} */}
+//          {/* {true ? <Card></Card> : null} */}
+//         </div>
+//     );
+   
+// }
+// export default App ;
 
-const App =() => {
-    const wishes ="hii anil"
-    return (
-        <div>
-            {/* <h1>Hello World</h1>
-        <ButtonComponent></ButtonComponent>
-        <ImageComponent></ImageComponent>
-        <Heading1></Heading1>
-         <ListParentOrdered></ListParentOrdered> */}
-         <h3>{wishes}</h3>
-         {[1].map((num,index)=>(<Card key={index}></Card>))}
-         {/* {false ? <Card></Card> : null} */}
-         {/* {true ? <Card></Card> : null} */}
-        
-        </div>
-    );
+import TableComponent from "./components/Tabel/Tabel.js";
+const App = () =>{
+    return(
+        <TableComponent />
+    )
 }
-export default App ;
+export default App;
--- src/components/Tabel/Tabel.js
+++ src/components/Tabel/Tabel.js
@@ -1,31 +1,28 @@
 // TableComponent.js
 import React from 'react';
-
-const TableComponent = ({ formDataList }) => {
+import "../Tabel/Tabel.css"
+const TableComponent = () => {
     return (
-        <div className="container">
-            <h2>Submitted Data:</h2>
-            <table className="table">
-                <thead className="thead-dark">
-                    <tr>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Password</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {formDataList.map((formData, index) => (
-                        <tr key={index}>
-                            <td>{formData.firstName}</td>
-                            <td>{formData.lastName}</td>
-                            <td>{formData.email}</td>
-                            <td>{formData.password}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+       <table>
+            <tr>
+                <th>sNo</th>
+                <th>Name</th>
+                <th>PhNumber</th>
+                <th>mail</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Anil</td>
+                <td>9390015040</td>
+                <td>anilkumar.boddeti@gmail.com</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Chinnu</td>
+                <td>8074549459</td>
+                <td>chinnu.boddeti@gmail.com</td>
+            </tr>
+       </table>
     );
 };
 

6b33fd5a8b5309d8d792f8693559af7f337898b9
Narasimha1506 <anilkuamr.boddeti@gmail.com>
2024-07-11 10:11:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 // import ButtonComponent from "./components/button/button.js";
-// import { ImageComponent } from "./components/images/images.js";
+import { ImageComponent } from "./components/images/images.js";
 // import {Heading1} from "./components/heading/heading.js";
 // import{ListParentOrdered} from "./components/list/list.js";
-// import { Card } from "./components/cards/card.js";
+import { Card } from "./components/cards/card.js";
+import { ListParentOrdered } from "./components/list/list.js";
 // const App =() => {
 //     // const wishes ="hii anil"
-//     return (
-//         <div>
-//             {/* <h1>Hello World</h1>
-//         <ButtonComponent></ButtonComponent>
-//         <ImageComponent></ImageComponent>
-//         <Heading1></Heading1>
-//          <ListParentOrdered></ListParentOrdered> */}
-//          {/* <h3>{wishes}</h3> */}
-//          {[1].map((num,index)=>(<Card key={index}></Card>))}
-//          {/* {false ? <Card></Card> : null} */}
-//          {/* {true ? <Card></Card> : null} */}
-//         </div>
-//     );
+//     const isLogin = true
+//     if(isLogin){
+//         return(
+//             <>
+//             <h3>IPL TEAMS</h3>
+//             {/* <ImageComponent /> */}
+//             {/* <Card /> */}
+//             <ListParentOrdered/> 
+//             </>
+//         );
+//     }
+//     else{
+//         return(
+//             <>
+//             <h3>please acces to the app</h3>
+//             </>
+//         );
+//     }
+    // return (
+    //     <div>
+    //         {/* <h1>Hello World</h1>
+    //     <ButtonComponent></ButtonComponent>
+    //     <ImageComponent></ImageComponent>
+    //     <Heading1></Heading1>
+    //      <ListParentOrdered></ListParentOrdered> */}
+    //      {/* <h3>{wishes}</h3> */}
+    //      {[1].map((num,index)=>(<Card key={index}></Card>))}
+    //      {/* {false ? <Card></Card> : null} */}
+    //      {/* {true ? <Card></Card> : null} */}
+    //     </div>
+    // );
    
 // }
 // export default App ;
 
-import TableComponent from "./components/Tabel/Tabel.js";
+// import TableComponent from "./components/Tabel/Tabel.js";
+// const App = () =>{
+//     return(
+//         <TableComponent />
+//     )
+// }
+// export default App;
+
 const App = () =>{
     return(
-        <TableComponent />
-    )
-}
+        <>
+        <h1>IPL TEAMS </h1>
+        <ListParentOrdered />
+        </>
+    );
+};
 export default App;
--- src/components/list/list.js
+++ src/components/list/list.js
@@ -13,67 +13,106 @@ export const ListParentOrdered = () => {
       </ul>
     );
   };
+  // const ListItems = () =>{
+  //   return(
+  //     <div>
+  //       <li>apple</li>
+  //       <li>banana</li>
+  //       <li>pineapple</li>
+  //       <li>mango</li>
+  //     </div>
+  //   )
+  // }
   
-  const ListItems = () => {
-    const iplTeams = [
+  // const ListItems = () =>{
+  //   const fruitlist  = ["apple","banana","pineapple","mango"]
+  //   return(
+  //     <div>
+  //       {
+  //         fruitlist.map((eachfruit)=>{
+  //           return <h3>{eachfruit} </h3>
+  //         })
+  //       }
+  //     </div>
+  //   )
+  // }
+
+  const ListItems = ()=>{
+    const iplteams =[
       {
-        teamName: "CSK",
-        teamPlayers: ["Dhoni", "Jadeja", "raina"],
-        won: 5,
-        wonYears: [2015, 2012, 2016, 2017],
-        teamsLogo: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        teamname:"Rcb",
+        teamplayers:["Faf du Plessis (C)","Glenn Maxwell","Virat Kohli","Rajat Patidar","Anuj Rawat"],
+        won: 0,
+        wonyears :[],
+        teamlogo:"https://th.bing.com/th?id=OIP.OBk7KaJxvCLUB5m8RGk6LwHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2"
       },
       {
-        teamName: "SRH",
-        teamPlayers: ["Cummins", "Jadeja", "raina"],
-        won: 5,
-        wonYears: [2015, 2012, 2016, 2017],
-        teamsLogo:
-          "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        teamname:"Srh",
+        teamplayers:["Aiden Markram (C)","Abdul Samad","Abhishek Sharma","Marco Jansen" ],
+        won: 3,
+        wonyears :["2008","2007","2006","2005"],
+        teamlogo:"https://th.bing.com/th/id/OIP.gxUXg5qD_aJjNgwJbRQLFAHaEK?w=316&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+
       },
       {
-        teamName: "RCB",
-        teamPlayers: ["Cummins", "Jadeja", "raina"],
-        won: 8,
-        wonYears: [],
-        teamsLogo: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+        teamname:"KKr",
+        teamplayers:["Jason Roy"," Shreyas Iyer ©"," Angkrish Raghuvanshi", "Rinku Singh "],
+        won: 5,
+        wonyears :["2012","2011","2010","2009"],
+        teamlogo:"https://th.bing.com/th/id/OIP.NRBGPt47RNSyByGz6Lnw5QHaEK?w=279&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+      },
+      {
+        teamname:"Csk",
+        teamplayers:["MS Dhoni", "Ravindra Jadeja", "Ruturaj Gaikwad", "Ajinkya Rahane", "Deepak Chahar"],
+        won: 6 ,
+        wonyears :["2015","2014","2013"],
+        teamlogo:"https://th.bing.com/th/id/OIP.pXoSFJjuBcFs4OlZnOj09wHaEK?w=291&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+      },
+      {
+        teamname:"MI",
+        teamplayers:["Hardik Pandya","Rohit Sharma","Dewald Brevis","Suryakumar Yadav","Ishan Kishan"],
+        won: 5,
+        wonyears :["2019","2016","2018"],
+        teamlogo:"https://th.bing.com/th?q=MI+India+Logo&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.1&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
+      },
+      {
+        teamname:"GT",
+        teamplayers:["Abhinav Sadarangani"," B. Sai Sudharsan", "Darshan Nalkande", "David Miller", "Jayant Yadav "],
+        won: 1,
+        wonyears :["2019"],
+        teamlogo:"https://th.bing.com/th/id/OIP.qqFNA-bTm4oUkGDUIIxIggAAAA?w=282&h=159&c=7&r=0&o=5&dpr=1.1&pid=1.7"
       },
     ];
-  
-    function filterData(iplTeams) {
-      const newTeams = iplTeams.filter((eachTeam) => eachTeam.won >= 5);
-      return newTeams;
-    }
-  
-    function countMatch(iplTeams) {
-      const newIplTeamsCount = iplTeams.reduce((acc, eachTeam) => {
-        return acc + eachTeam.won;
-      }, 0);
-      return newIplTeamsCount;
-    }
-  
+      function filterdata(iplteams){
+        const newteams = iplteams.filter((eachTeam) => eachTeam.won >=5)
+          return newteams;
+      }
+
+      function countMatch(iplteams){
+        const newiplteamscount = iplteams.reduce((acc, eachTeam) => {
+          return acc+eachTeam.won
+        },0) 
+        return newiplteamscount;
+      }
     return (
       <div>
-        <h3>Total count of matches {countMatch(iplTeams)}</h3>
-        {filterData(iplTeams).map((eachTeam) => {
-          return (
-            <div>
-              <h2>{eachTeam.teamName}</h2>
-              {/* <img src={eachTeam.teamsLogo} width={100} height={100} /> */}
-              {eachTeam.teamPlayers.map((eachPlayer) => {
-                return (
-                  <>
-                    <h6>{eachPlayer}</h6>
-                  </>
-                );
-              })}
-            </div>
-          );
-        })}
-        {/* {fruitList.map((eachFruit) => {
-          return <h4>{eachFruit}</h4>;
-        })} */}
+        <h3>Total Count of Mtaches {countMatch(iplteams)}</h3>
+          {filterdata(iplteams).map((eachTeam)=>{
+            return(
+              <div>
+                <h1>{eachTeam.teamname}</h1>
+                <img src={eachTeam.teamlogo} width={400} height={400}/>
+                { eachTeam.teamplayers.map((eachplayer )=>{
+                    return(
+                      <>
+                      <h6>{eachplayer}</h6>
+                      </>
+                    )
+                  })
+                }
+              </div>
+            )
+          })};
       </div>
-    );
-  };
-  
+    )
+  }
